87df8c93cbeb9898205a3a649863e9a55dfff471
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-06-28 16:22:06 +0300

    separating out react app into even smaller components

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,12 @@
+function Avatar(props) {
+
+    return (
+
+        <div>
+           <img className="circle-img"src={props.img} alt="avatar_img"/>
+
+        </div>
+    )
+}
+
+export default Avatar;
---
+++ src/components/Detail.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Detail(props) {
+
+    return (
+
+<p className="info">{props.detailInfo}</p>
+
+    )
+}
+
+export default Detail;
+
+
